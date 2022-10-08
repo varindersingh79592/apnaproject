3f86b8fd1c19d63c20dc16283608a03caabfcca9
varindersingh79592 <varindersingh79592@gmail.com>
2022-10-08 20:28:10 +0530

    only sign page and other boilerplate code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SignInPage from "./components/WelcomePages/SignInPage";
+function App() {
+  return (
+    <div className="App">
+      <SignInPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WelcomePages/SignInPage.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import SignIn from "./SignIn.css";
+
+function SignInPage() {
+  const [userName, updateUserName] = useState("");
+  const [password, updatePassword] = useState("");
+  const userNameUpdation = (event) => {
+    updateUserName(event.target.value);
+  };
+  const passwordUpdation = (event) => {
+    updatePassword(event.target.value);
+  };
+  return (
+    <form className="complete__form">
+      <div id="heading">
+        <h1>Sign In to avail our services</h1>
+        <h3>Please Enter your Details</h3>
+        <label>Username </label>
+        <input
+          type="text"
+          className="input_value"
+          placeholder="Enter your Username"
+          value={userName}
+          onChange={userNameUpdation}
+        ></input>
+        <br></br>
+        <br></br>
+        <br></br>
+        <label>Password </label>
+        <input
+          type="password"
+          className="input_value"
+          placeholder="Enter your password"
+          value={password}
+          onChange={passwordUpdation}
+        ></input>
+      </div>
+      <br></br>
+      <button type="submit">Sign In</button>
+    </form>
+  );
+}
+
+export default SignInPage;
